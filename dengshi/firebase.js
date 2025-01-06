db21c4cb24e75d9c23fd13ffb1e172133c3436c7
Mehreen Mallick <mehreen.mallick.fiona@g.bracu.ac.bd>
2025-01-06 13:32:01 +0600

    intialising firebase

---
+++ dengshi/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3O0B5KDZ8qMqgtZN5D6nH1gkWjS89wqU",
+  authDomain: "dengshi-66a9f.firebaseapp.com",
+  projectId: "dengshi-66a9f",
+  storageBucket: "dengshi-66a9f.firebasestorage.app",
+  messagingSenderId: "598112636729",
+  appId: "1:598112636729:web:e9c9190e141129ce6aa2e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
